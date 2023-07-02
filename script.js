976a51945f2004f3e6cab37076213acd74bc4b21
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-07-02 19:43:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // complete the given function
-
+let str = prompt("Enter Input")
 function palindrome(str){
-
+	let newStr = str.split('').reverse().join('');
+	if(newStr == str){
+		return true;
+	}else{
+		return false;
+	}
+}
+str = str.toLowerCase();
+let result = palindrome(str);
+if(result == true){
+alert('true');
+}else{
+	alert('false');
 }
 module.exports = palindrome
